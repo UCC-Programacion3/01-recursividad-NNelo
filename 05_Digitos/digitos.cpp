6b263ef99134cba35e554d59b20fdd7765929d71
NeloGino <nelogino@gmail.com>
2017-08-22 21:59:13 -0300

    ver caso base imprimir separado

--- 05_Digitos/digitos.cpp
+++ 05_Digitos/digitos.cpp
@@ -4,22 +4,27 @@
 using namespace std;
 
 unsigned int cociente(unsigned int a, unsigned int b) {
-  if(b==0) throw 5;
-  if(a<b)
-    return 0;
-  return 1+cociente(a-b, b);
-  
+    if (b == 0) throw 5;
+    if (a < b)
+        return 0;
+    return 1 + cociente(a - b, b);
+
 }
 
 unsigned int resto(unsigned int a, unsigned int b) {
-  if(b==0) throw 5;
-  if(a<b)
-    return a;
-  return resto(a-b ,b);
+    if (b == 0) throw 5;
+    if (a < b)
+        return a;
+    return resto(a - b, b);
 }
 
 void escribir_espaciado(unsigned int num) {
-  
+    if (num == 0)
+        cout << "";
+    else {
+        escribir_espaciado(num / 10);
+        cout << (num) % 10 << " ";
+    }
 }
 
 // ./test/run_test --gtest_filter=test_04_Digitos.*
--- 05_Digitos/main.cpp
+++ 05_Digitos/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "digitos.h"
 
+using namespace std;
+
 int main() {
     std::cout << "Ejercicio 01/05\n" << std::endl;
+    unsigned int a;
+    cin >> a;
+    escribir_espaciado(a);
     return 0;
 }
